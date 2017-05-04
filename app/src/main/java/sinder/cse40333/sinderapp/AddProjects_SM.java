66a451252662158ffb9be9cd94311a76a91b4219
Ivy Wang <jwang28@nd.edu>
2017-05-03 21:36:51 -0400

    changed addproj

--- app/src/main/java/sinder/cse40333/sinderapp/AddProjects_SM.java
+++ app/src/main/java/sinder/cse40333/sinderapp/AddProjects_SM.java
@@ -7,12 +7,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by apple on 3/28/17.
  */
 
 public class AddProjects_SM extends AppCompatActivity {
+	Button save_project;
+	EditText inputProjName;
+	EditText inputDate;
+	EditText inputDescrip;
+	EditText inputConInfo;
+	EditText inputNumVol;
+	String projName;
+	String date;
+	String descrip;
+	String conInfo;
+	String numVol;
+
 	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
@@ -20,6 +35,32 @@ public class AddProjects_SM extends AppCompatActivity {
 		Toolbar my_tool_bar_sm3 = (Toolbar) findViewById(R.id.toolbar_sm3);
 		setSupportActionBar(my_tool_bar_sm3);
 		my_tool_bar_sm3.setTitle("Add Project");
+
+		inputProjName = (EditText) findViewById(R.id.editProjName);
+		projName = inputProjName.getText().toString();
+
+		inputDate = (EditText) findViewById(R.id.editDate);
+		date = inputDate.getText().toString();
+
+		inputDescrip = (EditText) findViewById(R.id.editDescrip);
+		descrip = inputDescrip.getText().toString();
+
+		inputConInfo = (EditText) findViewById(R.id.editConInfo);
+		conInfo = inputConInfo.getText().toString();
+
+		inputNumVol = (EditText) findViewById(R.id.editNumVol);
+		numVol = inputNumVol.getText().toString();
+
+		save_project = (Button) findViewById(R.id.saveProj);
+		save_project.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//TODO: save info into database
+				//System.out.println(projName + date + descrip + conInfo + numVol);
+				startActivity(new Intent(AddProjects_SM.this, PastProjects_SM.class));
+			}
+
+		});
 	}
 
 	@Override
