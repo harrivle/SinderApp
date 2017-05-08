84a368aad6df6720746af756cdab2aac5a9518ac
Harrison Le <harrivle@gmail.com>
2017-05-08 11:12:25 -0400

    misc

--- app/src/main/java/sinder/cse40333/sinderapp/LoginActivity.java
+++ app/src/main/java/sinder/cse40333/sinderapp/LoginActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,12 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 public class LoginActivity extends BaseActivity {
 	private static final String TAG = "EmailPassword";
 	boolean checked = false;
