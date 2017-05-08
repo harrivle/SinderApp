ecf9ddd5ffb0f45f9ba7309be7a085c43011e5d5
Natalie Alvarez <nalvare1@nd.edu>
2017-05-08 11:57:08 -0400

    "Natalie: added snack bar to savedprojects_v"

--- app/src/main/java/sinder/cse40333/sinderapp/SavedProjects_V.java
+++ app/src/main/java/sinder/cse40333/sinderapp/SavedProjects_V.java
@@ -1,12 +1,16 @@
 package sinder.cse40333.sinderapp;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 /**
  * Created by apple on 3/27/17.
@@ -35,6 +39,7 @@ public class SavedProjects_V extends AppCompatActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int res_id = item.getItemId();
 
+		/*
 		if (res_id == R.id.add_v3a) {
 			//??
 
@@ -47,6 +52,35 @@ public class SavedProjects_V extends AppCompatActivity {
 		} else if (res_id == R.id.returntowelcome_v3a) {
 			startActivity(new Intent(SavedProjects_V.this, LoginActivity.class));
 		}
+		*/
+		//ScheduleAdapter scheduleAdapter = new ScheduleAdapter(getApplicationContext(), teams);
+		ListView saved_projectsListView = (ListView) findViewById(R.id.saved_projectsListView);
+		//saved_projectsListView.setAdapter(scheduleAdapter);
+		AdapterView.OnItemClickListener clickListener_saved_projects = new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout_v3b);
+				Snackbar snackbar = Snackbar.make(coordinatorLayout, "Delete Project? (Hit yes or wait for snackbar to close)", Snackbar.LENGTH_LONG);
+				snackbar.setAction("Yes", new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Snackbar.make(coordinatorLayout, "Project removed successfully!", Snackbar.LENGTH_LONG).show();
+
+					}
+
+				});
+				snackbar.show();
+
+				/*
+					//add code to delete project here!!!
+				 */
+
+
+
+			}
+
+		};
+		saved_projectsListView.setOnItemClickListener(clickListener_saved_projects);
 
 		return true;
 	}
